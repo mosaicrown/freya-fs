0ae4355fceeaf33d5be801a47734b21a22089088
Matthew Rossi <mattw.rossi@gmail.com>
2021-03-04 17:46:15 +0100

    Encrypt FreyaFS metadata based upon the given password

--- metadata.py
+++ metadata.py
@@ -1,7 +1,12 @@
 import base64
+import getpass
 import json
 import os
 import secrets
+
+import nacl.pwhash
+import nacl.secret
+
 
 class Info:
     def __init__(self, key=None, iv=None, size=None):
@@ -13,16 +18,34 @@ class Metadata:
     def __init__(self, path):
         self.path = path
 
-        read = {}
-        if os.path.isfile(path):
-            with open(path) as f:
-                read = json.load(f)
-        
+        # TODO: manage first access
+        pw = getpass.getpass("Password: ").encode("utf-8")
+        salt = b'\xd0\xe1\x03\xc2Z<R\xaf]\xfe\xd5\xbf\xf8u|\x8f'
+
+        # Generate the key
+        kdf = nacl.pwhash.argon2i.kdf
+        self.key = kdf(nacl.secret.SecretBox.KEY_SIZE, pw, salt)
+
         self.metadata = {}
-        for path, info in read.items():
-            key = base64.b64decode(info['key'].encode("ascii"))
-            iv = base64.b64decode(info['iv'].encode("ascii"))
-            self.metadata[path] = Info(key, iv, info['size'])
+        if os.path.isfile(path):
+            # Read the encrypted metadata
+            with open(path, 'r') as f:
+                content = f.read()
+                encrypted = base64.b64decode(content)
+
+            # Decrypt metadata
+            box = nacl.secret.SecretBox(self.key)
+            # TODO: add try catch on nacl.exceptions.CryptoError
+            plaintext = box.decrypt(encrypted)
+
+            # Read JSON metadata
+            read = json.loads(plaintext)
+
+            # Recreate metadata data structure
+            for path, info in read.items():
+                key = base64.b64decode(info['key'].encode("ascii"))
+                iv = base64.b64decode(info['iv'].encode("ascii"))
+                self.metadata[path] = Info(key, iv, info['size'])
 
     def __contains__(self, path):
         return path in self.metadata
@@ -61,6 +84,7 @@ class Metadata:
         del self.metadata[path]
 
     def dump(self):
+        # Convert metadata to JSON-like format
         to_write = {}
         for path, info in self.metadata.items():
             to_write[path] = {
@@ -68,5 +92,14 @@ class Metadata:
                 'iv': base64.b64encode(info.iv).decode("ascii"),
                 'size': info.size
             }
+        # Produce JSON string
+        plaintext = json.dumps(to_write)
+
+        # Encrypt metadata
+        box = nacl.secret.SecretBox(self.key)
+        encrypted = box.encrypt(plaintext.encode("utf-8"))
+
+        # Store encrypted metadata
         with open(self.path, 'w') as f:
-            json.dump(to_write, f)
+            content = base64.b64encode(encrypted).decode("ascii")
+            f.write(content)
